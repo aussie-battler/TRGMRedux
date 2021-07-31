7a82f6bb17e2f9398ecfd5eb6c25d849cdcb846c
Chase <29675446+theace0296@users.noreply.github.com>
2021-07-31 14:14:42 -0400

    Get list of existing workshop items

--- .github/upload-to-workshop/index.js
+++ .github/upload-to-workshop/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { startGroup, getInput, endGroup, setFailed } = require('@actions/core');
 const fsx = require('fs-extra');
 const path = require('path');
 const axios = require('axios').default;
@@ -26,34 +27,60 @@ const queryForTRGM = (cursor = '*') => {
 };
 
 (async () => {
-  let currentCursor = '*';
-  let finalResponse = {};
-  while (currentCursor) {
-    currentCursor = null;
-    try {
-      const res = await queryForTRGM(currentCursor);
-      if (
-        res?.status === 200 &&
-        res?.data &&
-        res?.data?.response &&
-        res?.data?.response?.publishedfiledetails &&
-        res?.data?.response?.publishedfiledetails.length > 0
-      ) {
-        currentCursor = res.data.response.next_cursor;
-        for (const publishedFileDetail of res.data.response.publishedfiledetails) {
-          if (publishedFileDetail.creator === THEACE0296_CREATOR_ID) {
-            finalResponse = { ...publishedFileDetail };
-            currentCursor = null;
-            break;
-          }
-        }
-      } else {
-        console.error(res.statusText);
-      }
-    } catch (error) {
-      console.error(error);
+  try {
+    startGroup('Getting list of files to upload...');
+    const releaseAssetsPath = getInput('path') || path.resolve(__dirname, '../../Mission-Templates/');
+    const releaseAssets = fsx
+      .readdirSync(path.normalize(releaseAssetsPath))
+      .filter(file => file.includes('TRGM-Redux'));
+    const releaseAssetsMap = {};
+    for (const releaseAsset of releaseAssets) {
+      releaseAssetsMap[`[Nightly] ${releaseAsset}.pbo`.toLowerCase().replace(/\x20/g, '%20')] = releaseAsset;
     }
-  }
+    endGroup();
 
-  console.debug(JSON.stringify(finalResponse, null, 2));
+    startGroup('Getting list of existing workshop items...');
+    let currentCursor = '*';
+    let existingFiles = [];
+    while (currentCursor) {
+      currentCursor = null;
+      try {
+        const res = await queryForTRGM(currentCursor);
+        if (
+          res?.status === 200 &&
+          res?.data &&
+          res?.data?.response &&
+          res?.data?.response?.publishedfiledetails &&
+          res?.data?.response?.publishedfiledetails.length > 0
+        ) {
+          currentCursor = res.data.response.next_cursor;
+          for (const publishedFileDetail of res.data.response.publishedfiledetails) {
+            if (
+              publishedFileDetail.creator === THEACE0296_CREATOR_ID &&
+              Object.keys(releaseAssetsMap).includes(publishedFileDetail.filename.toLowerCase())
+            ) {
+              existingFiles = [ ...existingFiles, releaseAssetsMap[publishedFileDetail.filename.toLowerCase()] ];
+              currentCursor = null;
+              break;
+            }
+          }
+        } else {
+          console.error(res.statusText);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    console.debug(JSON.stringify(existingFiles, null, 2));
+
+    endGroup();
+  } catch (error) {
+    console.error('An error occured while updating release assets:');
+    console.error(error.name);
+    console.error(error.message);
+    console.error(error.stack);
+    setFailed(error);
+    process.exit(2);
+  }
 })();
